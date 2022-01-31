08b1bcdbb4bb63281b58ec4bb564bae3cebff2fc
Débora Caires de Souza Moreira <debora.caires.mor@gmail.com>
2022-01-31 10:16:37 -0300

    remover usuarios inativos estabelecido

--- src/app.js
+++ src/app.js
@@ -116,7 +116,7 @@ app.get('/messages', async (req, res) => {
 
     const filteredMessages = messages.filter(message => (message.type === 'message' || message.to === user || message.from === user || message.to === 'Todos'));
 
-    res.send(filteredMessages)
+    res.send(filteredMessages.reverse())
   } catch (err) {
     res.sendStatus(500);
   }
@@ -143,5 +143,30 @@ app.post('/status', async (req, res) => {
     res.sendStatus(500);
   }
 });
+
+async function removeInactiveUsers () {
+  try {
+    const participants = await db.collection('participants').find().toArray();
+
+    for (let i = 0; i < participants.length; i++) {
+      if ((Date.now() - participants[i].lastStatus) > 10000) {
+        await db.collection('participants').deleteOne({ _id: new ObjectId(participants[i]._id) });
+        const messageUpdate = {
+          from: participants[i].name,
+          to: 'Todos',
+          text: 'sai da sala...',
+          type: 'status',
+          time: dayjs().format('HH:mm:ss'),
+        };
+        await db.collection('messages').insertOne(messageUpdate);
+      }
+    }
+  } catch (err) {
+    console.log(err)
+  }
+  
+}
+
+setInterval(removeInactiveUsers, 15000);
 
 app.listen(5000);
