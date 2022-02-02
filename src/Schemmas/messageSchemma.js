0491dc1a411a70805addcf063c6f9f6e3fd2d5b1
mateuskuritza <mateuskuritza@gmail.com>
2022-02-02 10:49:00 -0300

    Preparando revisão de código

--- src/Schemmas/messageSchemma.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const messageSchemma = Joi.object({
-    to: Joi.string().min(1).required(),
-    text: Joi.string().min(1).required(),
-    type: Joi.required(),
-});
-
-export default messageSchemma;
